e42fb7eb835f4b4c24c5d75bff97069f269318d7
SlawekLysakowski <slawku1@gmail.com>
2022-05-14 09:46:29 +0200

    init

---
+++ src/test/java/pl/edu/wszib/pizzamarket/PizzamarketApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.pizzamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzamarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
